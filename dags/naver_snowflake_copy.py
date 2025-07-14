ca71d0d3b7358e6acbb0c63047760dc38e90e31b
psy <psy915@swu.ac.kr>
2025-07-15 01:38:21 +0900

    Upload naver_snowflake_copy.py

---
+++ dags/naver_snowflake_copy.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2025, 7, 1),
+}
+
+with DAG(
+    dag_id='copy_parquet_to_snowflake',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+    tags=['team8', 'snowflake'],
+) as dag:
+
+    copy_into_snowflake = SnowflakeOperator(
+        task_id='copy_from_s3_parquet',
+        sql="""
+            COPY INTO processed.naver
+            FROM @naver_stage
+            FILE_FORMAT = (TYPE = PARQUET)
+            PATTERN = '.*\\.parquet$';
+        """,
+        snowflake_conn_id='team8_snowflake_conn',
+    )
